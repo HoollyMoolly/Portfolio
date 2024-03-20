12ece86f0631394496bef1fec674810bfd393ddf
kororit12 <korarit.zaa@gmail.com>
2024-03-20 15:36:59 +0700

    Finish Project Ver.1

--- src/scroll.js
+++ src/scroll.js
@@ -1,28 +1,10 @@
-// Enable hidden nav bar
-{
-    const myTopnav = document.querySelector("#myTopnav")
-    const nav = document.querySelector("nav");
-    let lastScrollY = window.scrollY;
-  
-    window.addEventListener("scroll", () => {
-      if (lastScrollY < window.scrollY) {
-        console.log("down");
-        if (myTopnav.classList === "topnav open"){
-          nav.classList.add("nav-menu--hidden");
-        } 
-        else{
-          nav.classList.add("nav--hidden");
-        }
-      } 
-      else {
-        console.log("up");
-        if (myTopnav.classList === "topnav"){
-          nav.classList.remove("nav-hidden");
-        } 
-        else{
-          nav.classList.remove("nav--hidden");
-        }
-      }
-      lastScrollY = window.scrollY;
-    });
+window.addEventListener("scroll", ()=>{
+  let windowY = window.scrollY;
+  let goToTop = document.getElementById("goToTop");
+  if(windowY > 867){
+    goToTop.style.display = "block";
   }
+  else{
+    goToTop.style.display = "none";
+  }
+})
